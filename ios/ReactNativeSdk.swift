90785193219ebe9292c1c8532791538d537b405d
Deniz Can <deniz@amani.tech>
2022-07-25 18:03:28 +0300

    Implement startAmaniSDKWithCredentials on iOS

--- ios/ReactNativeSdk.swift
+++ ios/ReactNativeSdk.swift
@@ -73,9 +73,27 @@ class RNAmaniSDK: NSObject, AmaniSDKDelegate {
       nvi = NviModel(documentNo: params["documentNo"] as! String, dateOfBirth: params["birthDate"] as! String, dateOfExpire: params["expireDate"] as! String)
     }
     nativeSDK.setDelegate(delegate: self)
-//    nativeSDK.set(token: params["token"] as! String, customer: customer)
     nativeSDK.set(server: params["server"] as! String, token: params["token"] as! String, customer: customer, nvi: nvi, sharedSecret: params["sharedSecret"] as? String ?? nil, useGeoLocation: params["geolocation"] as? Bool ?? false, language: params["lang"] as? String ?? "tr")
     currentCallback = responseFn
+   
+    if let currentlyPresentedVC = RCTPresentedViewController() {
+      DispatchQueue.main.async {
+        self.nativeSDK.showSDK(overParent: currentlyPresentedVC)
+      }
+    }
+  }
+  
+  @objc
+  func startAmaniSDKWithCredentials(_ params: NSDictionary, callback responseFn: @escaping RCTResponseSenderBlock) -> Void {
+    let customer = CustomerRequestModel(name: params["name"] as? String ?? "", email: params["email"] as? String ?? "", phone: params["phone"] as? String ?? "", idCardNumber: params["id"] as! String)
+    var nvi: NviModel? = nil
+    if (params["birthDate"] != nil && params["expireDate"] != nil && params["documentNo"] != nil) {
+      nvi = NviModel(documentNo: params["documentNo"] as! String, dateOfBirth: params["birthDate"] as! String, dateOfExpire: params["expireDate"] as! String)
+    }
+    nativeSDK.setDelegate(delegate: self)
+    nativeSDK.set(server: params["server"] as! String, userName: params["loginEmail"] as! String, password: params["password"] as! String, customer: customer, nvi: nvi, sharedSecret: params["sharedSecret"] as? String ?? nil, useGeoLocation: params["geolocation"] as? Bool ?? false, language: params["lang"] as? String ?? "tr")
+    currentCallback = responseFn
+    
    
     if let currentlyPresentedVC = RCTPresentedViewController() {
       DispatchQueue.main.async {
