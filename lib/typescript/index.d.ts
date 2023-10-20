3866ad7fa9816dadcf19b9b05f7093848e4f957b
Deniz Can <deniz@amani.tech>
2023-10-20 15:47:21 +0300

    fix: add missing type definitions and module maps

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,4 @@
+import type { SDKActivityResult, StartAmaniSDKWithTokenParams } from './types';
+export declare function multiply(a: number, b: number): Promise<number>;
+export declare function startAmaniSDKWithToken(params: StartAmaniSDKWithTokenParams, callback: (data: SDKActivityResult) => void): void;
+//# sourceMappingURL=index.d.ts.map
